27eaa4c104dc481b55aad7f1f0295dc182b320fa
qwm <1475809610@qq.com>
2022-09-20 00:33:18 +0800

    first commit

---
+++ test01/src/main/java/com/qwm/Test.java
@@ -0,0 +1,14 @@
+package com.qwm;
+
+/**
+ * User: 钱伟民
+ * Date: 2022/9/20
+ * Time: 0:27
+ * Description:
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
